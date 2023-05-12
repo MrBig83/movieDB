4c992c23c52cf1a687a1a789714858126f00e32d
MrBig83 <mjohansson176@gmail.com>
2023-05-12 11:56:44 +0200

    Component layout

---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+
+    return (
+        <div>
+            <h1>MovieDB</h1>
+            <div>
+                <p>NavLinks</p>
+            </div>
+        </div>
+    )
+}
+export default Header;
